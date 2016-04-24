99cdb1055a6072978f87ab52c60a8e2911807db9
mkg20001 <mkg20001@gmail.com>
2016-04-24 16:36:51 +0200

    Add Logging to cronjob

--- debian/small-cleanup-script.cron.d
+++ debian/small-cleanup-script.cron.d
@@ -1,4 +1,4 @@
 #
 # Regular cron jobs for the small-cleanup-script package
 #
-@daily root bash /usr/sbin/small-cleanup-script.sh
+@daily root bash /usr/sbin/small-cleanup-script.sh >> /var/log/small-cleanup-script.log
