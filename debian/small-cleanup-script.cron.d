3b38094d74120eed1313af561a27852498c3a6e6
mkg20001 <mkg20001@gmail.com>
2016-05-18 16:09:22 +0200

    Update cron.d

--- debian/small-cleanup-script.cron.d
+++ debian/small-cleanup-script.cron.d
@@ -1,4 +1,4 @@
 #
 # Regular cron jobs for the small-cleanup-script package
 #
-@daily root bash /usr/bin/small-cleanup-script 2>&1 >> /var/log/small-cleanup-script.log
+@daily root /usr/bin/small-cleanup-script &>> /var/log/small-cleanup-script.log
