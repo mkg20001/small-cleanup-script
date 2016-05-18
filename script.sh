ef580bbed81479584e8b270b5ff82f49e0f91389
mkg20001 <mkg20001@gmail.com>
2016-05-18 18:59:22 +0200

    Release 1.1.0 Fix 1

--- script.sh
+++ script.sh
@@ -28,15 +28,14 @@ rclist=`echo $(dpkg -l | awk '$1 == "rc" { print $2; }')`
 for f in $rclist; do
   log "PURGE" $f
 done
-if [ $rclist ]; then
+if [ "$rclist" ]; then
   apt-get remove --purge $rclist -y
 else
   log "Nothing to PURGE"
 fi
 
 #Snappy Stuff
-whereis snap 2> /dev/null > /dev/null
-if [ $? -eq 0 ]; then
+if [ -a /usr/bin/snap ]; then
   log "SNAP REFRESH"
   snap list | awk -F" " '{if ($1 && NR>1) { system("snap refresh " $1 " 2> /dev/null") }}'
 fi
